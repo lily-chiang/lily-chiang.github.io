9cc81051c142b1e51c44a84769ee83c3e65c0f79
Lily Chiang <lilyc@vt.edu>
2022-09-10 23:13:19 -0400

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     <script type="text/javascript" src="Scripts/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
-    <App />
+    <HashRouter><App /></HashRouter>
   </React.StrictMode>
 );
 
